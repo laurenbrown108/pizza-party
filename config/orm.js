00f25d9c3a16dd02123ca9c6912c330b2d48339a
Lauren Brown <laurenbrown108@gmail.com>
2020-10-08 22:53:48 -0400

    Some styling.

--- config/orm.js
+++ config/orm.js
@@ -53,24 +53,24 @@ const orm = {
             cb(result);
         });
     },
-    update: function(table, objColVals, condition, cb) {
+    update: function (table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
-    
+
         queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
         queryString += condition;
-    
+
         console.log(queryString);
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
         });
-      },
-    
+    },
+
 
 }
 
