20ff0d6eabc329034be5e5bb2bd567bcb2bc4e18
Lauren Brown <laurenbrown108@gmail.com>
2020-10-09 14:57:38 -0400

    modified connection to work w/ Heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASS,
-    database: "pizzas_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+    mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "pizzas_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
